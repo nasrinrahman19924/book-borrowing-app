3b5e4dc880e9a71a2d0f1365a3b3e1c41786e7f4
nasrinrahman19924 <nasrinrahman19924@gmail.com>
2026-05-12 22:21:33 +0600

    added Category page

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,7 +2,7 @@ import { Button } from "@heroui/react";
 import Link from "next/link";
 
 const Category = async () => {
-  const res = await fetch("https://pixgen-eta.vercel.app/category.json");
+  const res = await fetch("https://book-borrowing-app.vercel.app");
   const categories = await res.json();
   return (
     <div className="mb-5 space-x-3">
