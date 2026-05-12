f74b3777f6d222ceab0b369496febc6cef9d57ec
nasrinrahman19924 <nasrinrahman19924@gmail.com>
2026-05-12 21:02:29 +0600

    added category and profile page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
+import { Avatar } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+  const userData = authClient.useSession()
+  const user = userData.data?.user
   return (
     <div className="border-b px-2">
       <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
         <div className="flex gap-2 items-center">
-         
+
           <h3 className="font-black text-lg">📚 BookApp</h3>
         </div>
 
@@ -18,20 +22,30 @@ const Navbar = () => {
           <li>
             <Link href={"/all-books"}>All Books</Link>
           </li>
-        <li>
+          <li>
             <Link href={"/my-profile"}> My Profile</Link>
           </li>
         </ul>
 
         <div className="flex gap-6">
-          <ul className="flex items-center gap-4 text-sm">
+          {!user && <ul className="flex items-center gap-5 text-sm">
             <li>
               <Link href={"/signup"}>SignUp</Link>
             </li>
             <li>
               <Link href={"/signin"}>SignIn</Link>
             </li>
-          </ul>
+          </ul>}
+
+
+          {
+            user && <div>
+              <Avatar>
+                <Avatar.Image alt="John Doe" src={user?.image} />
+                <Avatar.Fallback>{user?.image[0]}</Avatar.Fallback>
+              </Avatar>
+            </div>
+          }
         </div>
       </nav>
     </div>
